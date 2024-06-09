0095cd0a73df6469b75184af5f56d9f9ce32d42c
park99999 <sungung1030@gmail.com>
2024-06-09 17:31:15 +0900

    fix & add : 동아리 리스트 화면 수정 , icon_heart 추가

--- app/src/main/java/com/example/kuclubapp/screens/ClubListScreen.kt
+++ app/src/main/java/com/example/kuclubapp/screens/ClubListScreen.kt
@@ -1,9 +1,13 @@
 package com.example.kuclubapp.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,6 +16,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -20,11 +26,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -54,9 +63,7 @@ fun ClubListScreen() {
     val context = LocalContext.current
 
     Column (
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize().background(Color(0xFFD9FDE8))
     ) {
         val clubItems = listOf(
             ClubItem(R.drawable.konkuk_logo, "중앙동아리", "목방", 13),
@@ -73,51 +80,63 @@ fun ClubListScreen() {
 @Composable
 fun ClubList(clubItems: List<ClubItem>) {
     LazyColumn {
-        items(clubItems){
-            ClubListItem(club = it)
+        itemsIndexed(clubItems) { index, club ->
+            val topPadding = if (index == 0) 31.dp else 15.dp
+            ClubListItem(club = club, topPadding = topPadding)
         }
     }
 }
+
 @Composable
-fun ClubListItem(club: ClubItem) {
+fun ClubListItem(club: ClubItem, topPadding: Dp) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
+            .padding(top = topPadding, start = 22.dp,end = 20.dp)
+            .height(100.dp)
+            .border(2.dp, Color(0xFFD0CCCC), shape = RoundedCornerShape(20.dp))
+            .clip(RoundedCornerShape(20.dp))
+            .background(Color.White)
+            .clickable {
+
+            }
     ) {
         Image(
             painter = painterResource(id = club.imageRes),
             contentDescription = null,
             modifier = Modifier
-                .size(64.dp)
-                .padding(end = 8.dp),
+                .size(78.dp)
+                .padding(12.dp)
+                .clip(RoundedCornerShape(200.dp)).align(Alignment.CenterVertically),
             contentScale = ContentScale.Crop
         )
-        Column(modifier = Modifier.weight(1f)) {
+        Column(modifier = Modifier.weight(1f).align(Alignment.CenterVertically)) {
             Text(
-                text = club.category,
+                text = club.category, fontSize = 14.sp,
                 color = Color.Gray
             )
             Text(
-                text = club.name,
+                text = club.name,fontSize = 18.sp,
                 color = Color.Black
             )
         }
+        Spacer(modifier = Modifier.weight(1f))
         Row(
-            verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
-            modifier = Modifier.padding(start = 8.dp)
+            modifier = Modifier.align(Alignment.Bottom).padding(bottom = 10.dp, end = 5.dp)
         ) {
             Icon(
-                painter = painterResource(id = R.drawable.baseline_mail_outline_24), //하트 아이콘으로 수정해야됨!!
+                painter = painterResource(id = R.drawable.icon_heart),
                 contentDescription = null,
                 tint = Color.Red,
-                modifier = Modifier.size(24.dp)
+                modifier = Modifier.size(24.dp).padding(end = 4.dp)
             )
             Text(
                 text = "${club.likes} likes",
-                color = Color.Black,
-                modifier = Modifier.padding(start = 4.dp)
+                color = Color.Black, fontSize = 14.sp, fontWeight = FontWeight.Bold ,
+                modifier = Modifier
             )
         }
     }
 }
+
+
