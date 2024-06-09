304d49226ee9e8bc840f6f7b80f451509fde9fdc
park99999 <sungung1030@gmail.com>
2024-06-09 19:29:42 +0900

    feat : 동아리상세화면 디자인 수정중

--- app/src/main/java/com/example/kuclubapp/screens/ClubDetailScreen.kt
+++ app/src/main/java/com/example/kuclubapp/screens/ClubDetailScreen.kt
@@ -4,25 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kuclubapp.data.ClubDetails
 import com.example.kuclubapp.R
 @Composable
 fun ClubDetailScreen() {
+    //추후에는 인자로 객체 전달 받기로 수정
     val clubDetails = ClubDetails(
         name = "GDSC Konkuk",
         imageRes = R.drawable.konkuk_logo,
@@ -50,7 +57,9 @@ fun ClubDetailScreen() {
         )
     )
     LazyColumn(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFD9FDE8))
     ) {
         item {
             Column(
@@ -59,17 +68,18 @@ fun ClubDetailScreen() {
                     .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = clubDetails.name)
+                Text(text = clubDetails.name, fontWeight = FontWeight.ExtraBold, fontSize = 27.sp,modifier = Modifier.padding(top = 15.dp))
                 Image(
                     painter = painterResource(id = clubDetails.imageRes),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(100.dp)
-                        .padding(top = 8.dp),
+                        .size(180.dp)
+                        .padding(12.dp)
+                        .clip(RoundedCornerShape(200.dp)),
                     contentScale = ContentScale.Crop
                 )
-                Text(text = clubDetails.category,  color = Color.Gray)
-                Text(text = clubDetails.type,  color = Color.Gray)
+                Text(text = clubDetails.category,  color = Color.Gray, fontSize = 16.sp,fontWeight = FontWeight.Bold)
+                Text(text = clubDetails.type,  color = Color.Gray,fontSize = 16.sp,fontWeight = FontWeight.Bold)
             }
         }
 
@@ -96,9 +106,40 @@ fun ClubInfoSection(clubDetails: ClubDetails) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .clip(RoundedCornerShape(15.dp, 15.dp, 0.dp, 0.dp))
+            .background(Color.White),
     ) {
-        Text(text = "정보")
+        Row(){
+            Column {
+                Text(
+                    text = "정보",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 20.dp, top = 20.dp)
+                )
+                Text(
+                    text = clubDetails.type,
+                    color = Color.Gray,
+                    fontSize = 13.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 20.dp)
+                )
+            }
+            Icon(
+                painter = painterResource(id = R.drawable.icon_heart),
+                contentDescription = null,
+                tint = Color.Red,
+                modifier = Modifier.size(24.dp).padding(top = 8.dp).align(Alignment.CenterVertically)
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.icon_heart),
+                contentDescription = null,
+                tint = Color.Red,
+                modifier = Modifier.size(24.dp).padding(top = 8.dp).align(Alignment.CenterVertically)
+            )
+
+
+        }
         Spacer(modifier = Modifier.height(8.dp))
         Text(text = "회장: ${clubDetails.president}")
         Text(text = "연락처: ${clubDetails.contact}")
