fe44da2056c296dab53f8821492d7750046b2b29
fusiontwo <yr0955@naver.com>
2024-06-20 02:52:08 +0900

    border 값 수정.

--- app/src/main/java/com/example/kuclubapp/screens/MypageScreen.kt
+++ app/src/main/java/com/example/kuclubapp/screens/MypageScreen.kt
@@ -1,12 +1,10 @@
 package com.example.kuclubapp.screens
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,20 +18,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -41,16 +32,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.example.kuclubapp.NavRoutes
 import com.example.kuclubapp.R
 import com.example.kuclubapp.firebaseDB.Clubs
 import com.example.kuclubapp.viewmodel.NavClubViewModel
@@ -178,7 +166,8 @@ fun likedClubUI(userId:String,  clubLikedList:List<Clubs>, navController: NavHos
                     .fillMaxWidth()
                     .padding(5.dp)
                     .background(Color.White, RoundedCornerShape(10.dp))
-                    .border(5.dp, Color.LightGray, RoundedCornerShape(10.dp))
+                    .border(2.dp, Color(0xFFD0CCCC), shape = RoundedCornerShape(20.dp))
+                    .clip(RoundedCornerShape(20.dp))
                     .height(100.dp)
                     .clickable {
                         navClubViewModel.selectedClub = index
